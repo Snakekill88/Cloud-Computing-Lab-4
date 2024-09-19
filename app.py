482886e86c770af30868f8e9b97664d01f17aa3d
root <root@AAD-PF4EEK45>
2024-09-19 15:01:00 -0500

    Initial flask-app

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from markupsafe import escape
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello Dominic!'
+
+@app.route('/<name>')
+def show_name(name):
+    # show the user profile for that user
+    return f'Hello {escape(username)}'
